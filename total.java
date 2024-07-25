5d7ead488bd064795b48a4d7b0fbbd2ebef6555b
junsunee <152352715+junsunee@users.noreply.github.com>
2024-07-25 14:49:15 +0700

    Create total.java

---
+++ total.java
@@ -0,0 +1,9 @@
+public class total {
+    public static void main(String args[]){
+        int x, y;
+        x = 62;
+        y = 99;
+        int total = x + y;
+        System.out.println("62+99 = " + total);
+    }
+}   
