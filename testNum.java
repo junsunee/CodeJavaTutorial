c4419e3128907ad34eccf921889d676c47a0c998
junsunee <152352715+junsunee@users.noreply.github.com>
2024-07-25 14:49:06 +0700

    Create testNum.java

---
+++ testNum.java
@@ -0,0 +1,9 @@
+public class testNum {
+    public static void main(String args[]){
+    int x, y; //ประกาศตัวแปรจำนวนเต็ม 
+    x =10;
+    y =x+1 ;
+    System.out.println("x =" +x+ "y = " +y);
+    }
+    
+}
