99ca31c79af1dcb0fedcf65995e6c44bf8b2e4d7
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-01 12:47:44 +0700

    Create Result1To100.java

---
+++ Result1To100.java
@@ -0,0 +1,10 @@
+public class Result1To100 {
+    public static void main(String[] args) {
+        int kung = 0, em = 1;
+        while (kung <= 100){
+           kung += em;
+            em++ ;
+    }
+    System.out.println("ผลลับ 1  ถึง 100 :"+kung);
+}
+}
