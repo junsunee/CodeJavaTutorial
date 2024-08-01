547bf23566e64937287cb7785e267c7e93b018f7
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-01 12:40:56 +0700

    Create TestSum2.java

---
+++ TestSum2.java
@@ -0,0 +1,12 @@
+public class TestSum2 {
+     public static void main(String[] args) {
+        int i,sum;
+        for(i = 1; i<= 100; i++) {
+            if(((1 % 3)==0) || ((1 % 5)==0))
+            System.out
+
+
+        }
+        
+     }
+}
