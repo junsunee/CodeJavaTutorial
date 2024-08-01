7e411341333ec49225954c1e56261096373c04d4
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-01 13:20:48 +0700

    Create TestFor.java

---
+++ TestFor.java
@@ -0,0 +1,9 @@
+public class TestFor {
+    public static void main(String[] args){
+      for (int i = 1; i <= 12; i++) {
+        int total;
+        total = 2 * 1;
+        System.out.println("2 x " + i + " = " +total);
+      }
+    }  
+}
