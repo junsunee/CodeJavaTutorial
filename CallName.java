d77f227ab5624231289ba3945c879dcb9c829afd
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-02 10:08:48 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,13 @@
+public class CallName {
+    public static void main(String[] args) {
+        System.out.println("Junsunee");
+        for(int i =1; i <10; i++){
+          Junsunee();
+      }
+      System.out.println("back in the main method.");
+  }
+  public static void Junsunee(){
+        System.out.println("Junsunee");
+  }
+  }
+
