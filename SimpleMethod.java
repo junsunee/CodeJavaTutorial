43765885546436ab444b94b2149889c5ce1e7391
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-02 10:09:04 +0700

    Create SimpleMethod.java

---
+++ SimpleMethod.java
@@ -0,0 +1,10 @@
+public class SimpleMethod {
+    public static void main(String[] args){
+        System.out.println("Hello from the main method.");
+        displayMessags();
+        System.out.println("Back in the main method.");
+}
+public static void displayMessags(){
+      System.out.println("Hello from the displayMessags method.");
+}
+}
