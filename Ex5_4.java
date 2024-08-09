a0f44f8a899a8db32df6f1b3ffc8b8402364ad4e
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-09 15:07:25 +0700

    Create Ex5_4.java

---
+++ Ex5_4.java
@@ -0,0 +1,10 @@
+public class Ex5_4 {
+        public static void main(String[] args) {
+        
+            System.out.println("เลขคู่จาก 2 ถึง 100 คือ:");
+            for (int i = 2; i <= 100; i += 2) {
+                System.out.println(i);
+            }
+        }
+    }
+    
