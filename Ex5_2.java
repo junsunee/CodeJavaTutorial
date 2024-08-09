8f76313e08a39f22d2c59ff16c72ce07912f4256
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-09 15:00:42 +0700

    Create Ex5_2.java

---
+++ Ex5_2.java
@@ -0,0 +1,10 @@
+public class Ex5_2 {
+    public static void main(String[] args) {
+        for(int x = 1; x <= 10; x++){
+            int cude = x * x * x ;
+            
+            System.out.println("The cude of " + x + " is : " + cude);
+        }
+    }
+}
+
