4b1a96b91e9f9ef0e60be9274e22f208fb888d94
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-09 15:20:50 +0700

    Update ifact.java

--- ifact.java
+++ ifact.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
-
-public class ifact {
-    public static void main(String[] args) { 
-        // สร้างอ็อบเจ็กต์ Scanner เพื่อรับค่าจากผู้ใช้ Scanner scanner = new Scanner(System.in); 
-        // รับค่าคะแนนจากผู้ใช้ System.out.print("Enter your score: ");
-         double score = scanner.nextDouble(); 
-         // เปรียบเทียบคะแนนและแสดงผลลัพธ์ if (score >= 60) { 
-         System.out.println("You are pass"); } 
-         else { System.out.println("You are not pass"); } 
-         // ปิด Scanner scanner.close(); 
-         }
-          }
+public class ifAct {
+    public static void main(String[] args) {
+        final int Poin = 60;
+        int inputpoin ;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your poin : ");
+        inputpoin = scan.nextInt();
+        System.out.println("You entered : " +inputpoin);
+        if(inputpoin > 60)
+        System.out.println("you are pass");
+        if(inputpoin < 60)
+        System.out.println("you are not pass");
+    }
 }
