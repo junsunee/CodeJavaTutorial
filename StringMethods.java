5ce68994a22f392013bf1d893133689905f05eeb
junsunee <152352715+junsunee@users.noreply.github.com>
2024-08-09 15:40:19 +0700

    Create StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,15 @@
+public class StringMethods {
+  public static void main(String[] args) {
+    String messags = "Java is great Fun";
+    String upper = messags.toLowerCase();
+    String Lower = messags.toLowerCase();
+    int SringSize = messags.length();
+    char letter = messags.charAt(3);
+
+    System.out.println(upper);
+    System.out.println(Lower);
+    System.out.println(SringSize);
+    System.out.println(letter);
+
+  }  
+}
